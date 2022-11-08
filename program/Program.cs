d60d9ee353a6b6d0c068c2517014198b066ab4b5
tim24ktk <tim24ktk@mail.ru>
2022-11-08 22:30:03 +0300

    program

---
+++ program/Program.cs
@@ -0,0 +1,45 @@
+﻿string[] strings = AskArray();
+string[] result = FillArray(strings, 3);
+
+Console.WriteLine();
+Console.WriteLine("Заданный и полученный массивы");
+Console.WriteLine($"[{string.Join(", ", strings)}] -> [{string.Join(", ", result)}]");
+
+string[] FillArray(string[] input, int n)
+{
+    string[] output = new string[CountLessThen(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThen(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+string[] AskArray()
+{
+    Console.WriteLine("Введите элементы через пробел!");
+    string inputStr = Console.ReadLine() ?? "";
+
+    return inputStr.Split(" ");
+}
